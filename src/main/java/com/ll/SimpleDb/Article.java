044eaece638613e5a62a84a92713fe664ab5686a
kknaks <benesia93@naver.com>
2025-01-17 09:45:13 +0900

    패키지 수정

--- src/main/java/com/ll/SimpleDb/Article.java
+++ src/main/java/com/ll/SimpleDb/Article.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.ll.SimpleDb;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/ll/SimpleDb/Sql.java
+++ src/main/java/com/ll/SimpleDb/Sql.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.ll.SimpleDb;
 
 import lombok.NoArgsConstructor;
 
